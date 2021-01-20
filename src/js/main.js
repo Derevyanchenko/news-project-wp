c0b9c4814c48f819f482d3e91254057f0c70c81d
Unknown <danilderevyanchenko@gmail.com>
2021-01-20 17:25:39 +0300

    finished all pages (need to test)

--- src/js/main.js
+++ src/js/main.js
@@ -19,4 +19,28 @@ $(document).ready(function() {
 
 	});
 
+
 });
+
+$(window).on("load resize", function() {
+
+	let previousText = $(".nav-previous").find(".nav-title");
+	let nextText = $(".nav-next").find(".nav-title");
+
+	let previousTextForPc = previousText.attr("data");
+	let previousTextForMobile = previousTextForPc.replace(" статья", "");
+
+	let nextTextTextForPc = nextText.attr("data");
+	let nextTextForMobile = nextTextTextForPc.replace(" статья", "");
+
+	if( $(window).width() <= 767 ) {
+		
+		previousText.text(previousTextForMobile);
+		nextText.text(nextTextForMobile);
+  
+	} else {
+		previousText.text(previousTextForPc);
+		nextText.text(nextTextTextForPc);
+	}
+  
+  });
